be7fd8c257ba36964d41088df09a2ce64917d137
David Roby <daroby@crimson.ua.edu>
2020-10-11 22:42:37 -0500

    Prep HTML documents, began work on login framework

---
+++ login.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- Local CSS -->
+	<link href="lstyle.css" rel="stylesheet" type="text/css">
+	
+<title>SLEC Music DL | Login</title>
+</head>
+<body>
+	
+<!--Banner-->
+<nav class="navbar navbar-light bg-light"> <a class="navbar-brand" href="#"> <img src="https://www.saint-lukes.com/themes/1/images/logo-fixed.png" height="40" alt=""> </a> </nav>
+
+<div class="container-xl">	
+<div class="mt-4 row">
+    <div class="p-4 col-lg-6">
+		<h2>Saint Luke's Episcopal Church</h2>
+		<h5>Digital Music Library</h5>
+		
+			<p>The purpose of this repository is to 1) function as a catalog for the physical music library, 2) provide discovery tools for the selection of liturgically appropriate musical works, and 3) provide full digital access to all materials held in the Saint Luke's Music Library in PDF form.</p>
+
+			<p>DISCLAIMER: By using this digital library, you agree to refrain from sharing, distributing, or copying any works that may be protected under U.S. copyright law.</p>
+	</div>
+    <div class="col-lg-6 p-4 border">
+		<h4>Please sign in.</h4>
+	<form class="mt-3">
+ 		<div class="form-group row">
+ 			<label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+    		<div class="col-sm-10">
+      			<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+   			</div>
+  		</div>
+  		<div class="form-group row">
+    		<label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+    		<div class="col-sm-10">
+      			<input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+    		</div>
+  		</div>
+  		<div class="form-group row">
+    			<div class="col-sm-4">
+      				<div class="form-check">
+        				<input class="form-check-input" type="checkbox" id="gridCheck1">
+        				<label class="form-check-label" for="gridCheck1"> Remember me </label>
+      				</div>
+    			</div>
+  			</div>
+  		<div class="form-group row">
+    		<div class="col">
+      			<button type="submit" class="btn btn-primary">Sign in</button>
+    		</div>
+			<div class="col-7 text-right">
+	  			<p>Need an account? <a href="#">Create one here.</a></p>
+			</div>
+  		</div>
+	</form>
+	</div>
+</div>	
+</div>	
+
+<!--Footer-->
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
