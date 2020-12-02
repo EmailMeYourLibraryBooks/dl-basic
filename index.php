3ba413377649b1ebf9a6a9eff5001db9881ecff0
David Roby <david.roby@hey.com>
2020-12-02 13:45:29 -0600

    added favicon to head

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+	<link rel="icon" href="https://saint-lukes.com/wp-content/uploads/2020/08/Asset-13.png">
     <title>Home</title>
   </head>
 	
